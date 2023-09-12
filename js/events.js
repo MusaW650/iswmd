fd4655a789be5dfaf04355cb0fc547213496bcb5
Musa Waseem <musaw650@gmail.com>
2023-09-11 22:30:21 -0400

    updating timings and flyers

--- js/events.js
+++ js/events.js
@@ -1,69 +1,88 @@
+
+
 function renderEvents(events) {
-    console.log("renderEvents")
+  console.log("renderEvents");
 
-    renderer = document.getElementById("renderer")
-    events.forEach(element => {
-        div = document.createElement("div")
-        div.class = "card h-100"
-        
-        cardBody = document.createElement("div")
-        cardBody.class = "card-body"
-        
-        title = document.createElement("h3")
-        title.innerText = element.title
+  renderer = document.getElementById("renderer");
+  events.forEach((element) => {
+    div = document.createElement("div");
+    div.class = "card h-100";
 
-        date = document.createElement("h4")
-        date.innerText = element.date === "" ? "" : "Date: " + element.date
+    cardBody = document.createElement("div");
+    cardBody.class = "card-body";
 
-        description = document.createElement("h5")
-        description.innerText = element.description
+    title = document.createElement("h3");
+    title.innerText = element.title;
 
-        cardBody.appendChild(title)
-        cardBody.appendChild(date)
-        cardBody.appendChild(description)
-        
-        div.appendChild(cardBody)
-        div.appendChild(document.createElement("hr"))
+    date = document.createElement("h4");
+    date.innerText = element.date === "" ? "" : "Date: " + element.date;
 
-        renderer.appendChild(div)
-    });
+    description = document.createElement("h5");
+    description.innerText = element.description;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(date);
+    cardBody.appendChild(description);
+
+    div.appendChild(cardBody);
+    div.appendChild(document.createElement("hr"));
+
+    renderer.appendChild(div);
+  });
 }
 
 function renderThisWeek(events) {
-    console.log("renderThisWeek")
+  console.log("renderThisWeek");
 
-    var i = 1
-    events.forEach(event => {
-        if (event.title.includes("\n")) event.title = "That's all for this week!"
-        document.getElementById(`thisweek${i}title`).innerText = event.title
-        document.getElementById(`thisweek${i}date`).innerText = event.date
-        i++ 
-    }) 
-} 
-
+  var i = 1;
+  events.forEach((event) => {
+    if (event.title.includes("\n")) event.title = "That's all for this week!";
+    document.getElementById(`thisweek${i}title`).innerText = event.title;
+    document.getElementById(`thisweek${i}date`).innerText = event.date;
+    i++;
+  });
+}
+var myLink = link("See form below for timings", "https://docs.google.com/forms/d/e/1FAIpQLSc3ElhtahCHoDIjxgGYvCKhy62wzTNLnP9xlRPMadYVRpiEew/viewform");
 events = [
-    {
-        title: "Jummah khutba by Sh Tajani",
-        description: "",
-        date: "Fri, May 19, 1:30pm - 2:15pm"
-    },
-    {
-        title: "Evening halaqa with Sh Tajani",
-        description: "",
-        date: "Fri, May 12, 7pm - 8pm"
-    },
-    {
-        title: "New Muslims Class!",
-        description: "",
-        date: "Every Monday and Thursday at 8pm"
-    },
-    {
-        title: "\n\n\n\n\n\n\n\n\n",
-        description: "",
-        date: ""
-    },
-]
+  {
+    title: "Jummah khutba and evening halaqa by Sh Abdullahi",
+    description: "",
+    date: "Jummah: Fri, September 15, 1:30pm - 2:15pm\nEvening Halaqa: after Maghrib",
+  },
+  
+  {
+    title: "Seerah Class With Brother Abdul Waheed",
+    description: "",
+    date: "Every Wednesday after Maghrib",
+  },
+  {
+    title: "Adult and Children Quran Classes",
+    description: "",
+    date: myLink.textContent,
+  },
+  {
+    title: "\n\n\n\n\n\n\n\n\n",
+    description: "",
+    date: "",
+  },
+];
 
-var page = window.location.pathname.split("/").pop()
-if (page === "events.html" || page === "events") renderEvents(events) // just events in prod
-else if (page === "index.html" || page === "") renderThisWeek(events) // empty string in prod
+function link(text, href) {
+    var link = document.createElement("a");
+
+    // Set the text content for the link
+    link.textContent = text;
+    
+    // Set the href attribute for the link
+    link.href = href;
+    
+    // Append the link to a container element in the DOM
+   // document.body.appendChild(link);
+    return link;  
+}
+
+var page = window.location.pathname.split("/").pop();
+if (page === "events.html" || page === "events")
+  renderEvents(events); // just events in prod
+else if (page === "index.html" || page === "") renderThisWeek(events); // empty string in prod
+
