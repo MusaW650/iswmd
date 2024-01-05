101d2cd487f734083379d0b3fbc8826fa64a3059
AaqibMasood <144629629+AaqibMasood@users.noreply.github.com>
2024-01-04 19:19:54 -0500

    Updated jummah time and removd outdated flyer

--- js/events.js
+++ js/events.js
@@ -48,7 +48,7 @@ events = [
   {
     title: "Jummah khutba and evening halaqa",
     description: "",
-    date: "Jummah: Fri, December 29, 𝟭:𝟯𝟬𝗽𝗺\nIsha: 7:30\n Halaqa: 7:45\nDinner to follow",
+    date: "Jummah: Fri, January 5, 𝟭:𝟯𝟬𝗽𝗺\nIsha: 7:30\n Halaqa: 7:45\nDinner to follow",
   },
   
   {
