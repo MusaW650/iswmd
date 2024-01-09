f94fe659484a1887a07eb1c14fff940647446344
AaqibMasood <144629629+AaqibMasood@users.noreply.github.com>
2024-01-08 21:37:53 -0500

    Updated jummah time

--- js/events.js
+++ js/events.js
@@ -48,7 +48,7 @@ events = [
   {
     title: "Jummah khutba and evening halaqa",
     description: "",
-    date: "Jummah: Fri, January 5, 𝟭:𝟯𝟬𝗽𝗺\nIsha: 7:30\n Halaqa: 7:45\nDinner to follow",
+    date: "Jummah: Fri, January 12, 𝟭:𝟯𝟬𝗽𝗺\nIsha: 7:30\n Halaqa: 7:45\nDinner to follow",
   },
   
   {
